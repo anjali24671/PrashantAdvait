f07cd0f5cc7de42954539981632116a426c9fad5
Anjali <holla2467@gmail.com>
2024-06-22 11:07:15 +0530

    added section-1 and made it responsive

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,12 +24,12 @@
 
 </script>
 
-<nav class="flex text-sm font-sans font-[550] items-center space-x-100 justify-between h-10 bg-orange-600">
-    
+<nav  class="w-full sticky top-0 z-50 text-white flex justify-between items-center bg-orange-600 h-[40px]">
+    <div class="flex justify-between gap-[10px]">
         <div><a href="/">Home</a></div>
         <div>
             <select class="bg-orange-600" id="sessionSelect" on:click={goto} bind:value={selected}>
-                <option value="" disabled selected hidden>live session</option>
+                <option value="" disabled selected hidden>Live Session</option>
                 <option value="/gita">Gita</option>
                 <option value="/vedant-basics-to-classic">Vedant</option>
             </select>
@@ -37,16 +37,24 @@
         <div><a href="/video-modules">Video Series</a></div>
         <div><a href="/books">AP Books</a></div>
         <div><a href="/articles">AP articles</a></div>
-		<select class="bg-orange-600" id="invite" on:click={goto} bind:value={selected}>
+		<select class="bg-orange-600 w-[65px]" id="invite" on:click={goto} bind:value={selected}>
 			<option value="" disabled selected hidden>invite</option>
 			<option value="/invite/talks">for a talk</option>
 			<option value="/invite/podcasts">for an interview</option>
 		</select>
-        <div><a href="/contribute">donate</a></div>
-    
-
-    <div>
-        <button on:click={toggleExpand} id="menu">Menu
+        <div><a href="/contribute">Donate</a></div>
+    </div>
+  
+    <div class="flex justify-between space-x-[10px] space-r-[10px] items-center">
+        <a href="/carts">
+            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="19" viewBox="0 0 23 19" class="fill-current">
+                <path d="M8.094 14.263h11.568a.8.8 0 0 0 .567-.223.74.74 0 0 0 .241-.569.74.74 0 0 0-.241-.568.8.8 0 0 0-.567-.223H8.293a.85.85 0 0 1-.63-.243 1.13 1.13 0 0 1-.304-.64L5.763 1.188Q5.67.599 5.37.299 5.07 0 4.272 0H.83a.8.8 0 0 0-.577.243A.78.78 0 0 0 0 .822q0 .334.252.573t.577.238h3.244L5.627 11.99q.168 1.056.76 1.664.594.609 1.707.609m-1.785-3.47h13.405q1.124 0 1.717-.613.593-.615.76-1.68l.767-4.94.026-.202A1.4 1.4 0 0 0 23 3.175q0-.375-.268-.614-.267-.238-.73-.238H5.428l.021 1.572h15.673l-.661 4.443a1.16 1.16 0 0 1-.29.635q-.225.237-.624.238l-13.248.01zM8.881 19q.714 0 1.197-.472.482-.472.482-1.14 0-.67-.482-1.142-.483-.472-1.197-.472-.693 0-1.181.472a1.53 1.53 0 0 0-.488 1.141q0 .67.488 1.141.488.472 1.18.472m9.248 0q.693 0 1.181-.472t.488-1.14-.488-1.142a1.64 1.64 0 0 0-1.18-.472q-.694 0-1.192.472a1.5 1.5 0 0 0-.499 1.141q0 .67.499 1.141.499.472 1.191.472"></path>
+            </svg>   
+        </a>
+        <button on:click={toggleExpand} id="menu">
+            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="16" viewBox="0 0 22 16" class="fill-current">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.826" d="M1 1.609h20M1 8h20M1 14.391h20" style="--darkreader-inline-stroke: currentColor;" data-darkreader-inline-stroke=""></path>
+            </svg>
             {#if expand}
                 <div>I am expanded</div>
                 {#if data.authenticated}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,9 @@
     import { PUBLIC_USERFRONT_ACCOUNT_ID, PUBLIC_USERFRONT_PUBLIC_KEY_BASE64, PUBLIC_KEY_ID, PUBLIC_KEY_SECRET } from '$env/static/public';
     export let data
   
+    console.log(data.articleCategories)
+
+
     let rzp1;
     let razorpayScriptLoaded = writable(false);
   
@@ -76,7 +79,51 @@
 
 
   </script>
-  
+
+  <div class="flex bg-gray-800 text-white justify-between items-center lg:text-m sm:text-sm">
+    <div class="flex gap-2 items-center">
+      <div class="border border-white rounded-[8px] bg-red-600 w-3 h-3"></div>
+      Acharya Prashant is dedicated to building a brighter future for you 
+      </div>
+    <button class="bg-transparent px-2 h-6 rounded-[3px] border border-white">Extend your hand</button>
+  </div>
+
+
+  <!-- SECTION 1 -->
+  <div  class="relative">
+    <!-- <img src="https://cimg.acharyaprashant.org/images/img-67be009c-628a-4d15-a2bb-ba5bfc615a97/30/image.jpg"> -->
+    <picture>
+      <source srcset="https://cimg.acharyaprashant.org/images/img-599af525-d2a4-4741-b1e2-78b74e7d342b/20/image.jpg" media="(max-width: 1024px)">
+      <source srcset="https://cimg.acharyaprashant.org/images/img-67be009c-628a-4d15-a2bb-ba5bfc615a97/30/image.jpg" media="(min-width: 1024px)">
+      <img src="https://cimg.acharyaprashant.org/images/img-67be009c-628a-4d15-a2bb-ba5bfc615a97/30/image.jpg" alt="Responsive image" class="w-full h-auto">
+    </picture>
+
+   
+      <div class="flex px-[30px]  justify-around hidden lg:block w-[50%] h-[100%] absolute top-1/4 flex-col items-center " >
+        <h1 class="text-xl my-4 font-semibold" style=" font-size: clamp(3rem, 4.8vw, 4.5rem)">Acharya Prashant</h1>
+        <h2 class="text-[30px] font-normal italic" style="font-size: clamp(2rem, 2.8vw, 2.6rem);">To demolish all that is false</h2>
+        <p class="text-sm">
+          Acharya Prashant is a spiritual teacher rooted in Advait Vedanta. His unique spiritual literature is at par with the highest words that mankind has ever known. Equally, one could simply call him a teacher beyond any tradition.<br>
+  He is a veganism promoter, an environmental activist, a science activist, a campaigner against superstition, and a champion of essential human freedom.
+        </p>
+        <span class="text-sm text-orange-600">Read Bio -</span>
+      </div>
+
+
+    <div class="flex lg:hidden flex-col items-center">
+      <h1 class="text-[70px] font-semibold">Acharya Prashant</h1>
+      <h2 class="text-[45px] font-normal italic">To demolish all that is false</h2>
+      <p>
+        Acharya Prashant is a spiritual teacher rooted in Advait Vedanta. His unique spiritual literature is at par with the highest words that mankind has ever known. Equally, one could simply call him a teacher beyond any tradition.<br>
+He is a veganism promoter, an environmental activist, a science activist, a campaigner against superstition, and a champion of essential human freedom.
+      </p>
+      <span class="text-sm text-orange-600">Read Bio -</span>
+    </div>
+  </div>
+
+
+
+
   <main>
     <h1>Pay with Razorpay</h1>
     {#if $razorpayScriptLoaded}
