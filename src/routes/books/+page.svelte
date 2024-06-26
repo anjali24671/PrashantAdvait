a2e098fcac4731170422c47809b44d638c62e6f1
Anjali <holla2467@gmail.com>
2024-06-26 14:44:26 +0530

    made header section of book page

--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -1,4 +1,23 @@
-<h1>books</h1>
+<script>
+    export let data
+    console.log(data.loadBestSeller.bookPrice)
 
+</script>
 
-<a href="/cart/book">book</a>
+<div class="px-10">
+    <div class="flex gap-4 w-[660px] flex-col">
+        <div class="flex  w-[660px]">
+            <input class="w-[90%] p-2" placeholder="search for book">
+            <button class="bg-orange-200 p-2">Search</button>
+        </div>
+        <div class="flex gap-4">
+            <div class="border font-semibold flex-shrink-0 border-gray-600 py-1 px-2 rounded-[8px]">All</div>
+            <div class="border font-semibold flex-shrink-0 border-gray-600 py-1 px-2 rounded-[8px]">Explore</div>
+            <div class="border font-semibold flex-shrink-0 border-gray-600 py-1 px-2 rounded-[8px]">Categories</div>
+            <div class="border font-semibold flex-shrink-0 border-gray-600 py-1 px-2 rounded-[8px]">Offers</div>
+            <div class="border font-semibold flex-shrink-0 border-gray-600 py-1 px-2 rounded-[8px]">My Books</div>
+            <div class="border font-semibold flex-shrink-0 border-gray-600 py-1 px-2 rounded-[8px]">My Orders</div>
+            <div class="border font-semibold flex-shrink-0 border-gray-600 py-1 px-2 rounded-[8px]">Contact Us</div>
+        </div>
+    </div>
+</div>
