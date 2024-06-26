7a649ea55243ad41da99bf7d78e83e8884433f3f
Anjali <holla2467@gmail.com>
2024-06-26 14:54:22 +0530

    made book header responsive

--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -4,11 +4,13 @@
 
 </script>
 
-<div class="px-10">
-    <div class="flex gap-4 w-[660px] flex-col">
-        <div class="flex  w-[660px]">
-            <input class="w-[90%] p-2" placeholder="search for book">
-            <button class="bg-orange-200 p-2">Search</button>
+<header class="px-10">
+    <div class="flex gap-4 flex-col">
+        <div class="flex rounded-[8px] overflow-hidden md:w-[660px] relative">
+            <input class="w-full p-2 md:px-2 px-[40px] md:rounded-[0px] rounded-[8px]" placeholder="search for book">
+            <button class="md:bg-orange-200 p-2 absolute h-full md:right-0">
+                <svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16" class="fill-current laptop:h-4 laptop:w-4"><path fill-rule="evenodd" d="M6.5 2a4 4 0 1 0 0 8 4 4 0 0 0 0-8m-6 4a6 6 0 1 1 10.89 3.476l4.817 4.817a1 1 0 0 1-1.414 1.414l-4.816-4.816A6 6 0 0 1 .5 6"></path></svg>
+            </button>
         </div>
         <div class="flex gap-4">
             <div class="border font-semibold flex-shrink-0 border-gray-600 py-1 px-2 rounded-[8px]">All</div>
@@ -20,4 +22,4 @@
             <div class="border font-semibold flex-shrink-0 border-gray-600 py-1 px-2 rounded-[8px]">Contact Us</div>
         </div>
     </div>
-</div>
+</header>
