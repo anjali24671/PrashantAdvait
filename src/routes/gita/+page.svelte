4c4619127408c3bbb1f5ddaa9718d15b09197e6c
Anjali <holla2467@gmail.com>
2024-06-28 00:20:40 +0530

    basic upper section for gita page

--- src/routes/gita/+page.svelte
+++ src/routes/gita/+page.svelte
@@ -1,1 +1,29 @@
-<h1>Gita</h1>
+<section class="flex flex-col justify-center items-center">
+    <div><h1>LIVE BHAGAWAD GITA</h1></div>
+    <div class="self-center">Acharya Prashant is teaching Bhagawad Gita, verse by verse.
+    Do not miss</div>
+    <div class="flex md:flex-row flex-col max-w-sm rounded-[5px] border border-orange-600 p-3 justify-center items-start dm:items-center">
+        <p>Your enrolment for Gita Samagam is expiriing in 4 days. Please re-enrol for the next month</p>
+        <div class="bg-red-400 p-1 px-2 w-[120px] h-[35px]  rounded-[7px]">
+            <button>Re-Enrol now</button>
+        </div>
+    </div>
+    <div class="flex justify-around">
+        <div>
+            <h1>30000+</h1>
+            <p>Participants attend live classes together from across the globe</p>
+        </div>
+        <div>
+            <h1>1,30,000+</h1>
+            <p>Active users on Gita Community</p>
+        </div>
+        <div>
+            <h1>3000+</h1>
+            <p>Questions answered. Opportunity to ask questions from Acharya Prashant.</p>
+        </div>
+        <div>
+            <h1>300+</h1>
+            <p>Sessions conducted. 30 live sessions every month. Get access to all past sessions.</p>
+        </div>
+    </div>
+</section>
