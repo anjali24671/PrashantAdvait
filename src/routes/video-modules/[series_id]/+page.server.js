64d6a0fbc0e6855cf1684b56bece932c7adaa3c1
Anjali <holla2467@gmail.com>
2024-06-27 18:35:49 +0530

    made the basic responsive structure for video-module-detail page

---
+++ src/routes/video-modules/[series_id]/+page.server.js
@@ -0,0 +1,28 @@
+import VideoSeries from "$lib/database/VideoSeries";
+import connect from "$lib/database/connection";
+
+export async function load({ params }) {
+
+    try {
+        await connect()
+
+        const series_id = params.series_id
+        // find the video series for the given (params) videoCategory
+        const videoSeriesResponse = await VideoSeries.find({ category_id: series_id }) 
+        let videoSeries = {}
+        videoSeries["videoSeries"] = videoSeriesResponse
+
+        const series = JSON.stringify(videoSeries)
+
+        return {
+            series
+        }
+    } catch (e) {
+        return {
+            status: 401,
+            message: e.message
+        };
+    }
+    
+   
+}
