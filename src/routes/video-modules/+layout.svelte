1599f645ae7fbc65a509803b146a6a80669aed68
Anjali <holla2467@gmail.com>
2024-07-08 14:29:52 +0530

    make basic search bar design for video-modules

--- src/routes/video-modules/+layout.svelte
+++ src/routes/video-modules/+layout.svelte
@@ -1,18 +1,27 @@
-<header>
-    <div>
-        <img src="#">
-    </div>
 
-    <div>
-        <div>
-            <input placeholder="Search for video series">
+
+<div>
+    <header class="mx-4 my-2 flex gap-2">
+        <div><h2>Video series</h2></div>
+        <div class="flex gap-4 flex-col border overflow-hidden rounded-[8px]">
+            <div class="flex md:w-[660px] relative">
+                <select class="px-2 border-r w-[50px]">
+                    <option value="" disabled selected hidden>All</option>
+                    <option>hello</option>
+                    <option>hello2</option>
+                </select>
+                <input  class="w-full p-2 md:px-2 px-[40px] md:rounded-[0px] rounded-[8px]" placeholder="search for videos">
+                <button  class="md:bg-orange-200 p-2 absolute h-full md:right-0">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16" class="fill-current laptop:h-4 laptop:w-4"><path fill-rule="evenodd" d="M6.5 2a4 4 0 1 0 0 8 4 4 0 0 0 0-8m-6 4a6 6 0 1 1 10.89 3.476l4.817 4.817a1 1 0 0 1-1.414 1.414l-4.816-4.816A6 6 0 0 1 .5 6"></path></svg>
+                </button>
+            
+                
+            </div>
+            
         </div>
-        <div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="19" viewBox="0 0 23 19" class="fill-current"><path d="M8.094 14.263h11.568a.8.8 0 0 0 .567-.223.74.74 0 0 0 .241-.569.74.74 0 0 0-.241-.568.8.8 0 0 0-.567-.223H8.293a.85.85 0 0 1-.63-.243 1.13 1.13 0 0 1-.304-.64L5.763 1.188Q5.67.599 5.37.299 5.07 0 4.272 0H.83a.8.8 0 0 0-.577.243A.78.78 0 0 0 0 .822q0 .334.252.573t.577.238h3.244L5.627 11.99q.168 1.056.76 1.664.594.609 1.707.609m-1.785-3.47h13.405q1.124 0 1.717-.613.593-.615.76-1.68l.767-4.94.026-.202A1.4 1.4 0 0 0 23 3.175q0-.375-.268-.614-.267-.238-.73-.238H5.428l.021 1.572h15.673l-.661 4.443a1.16 1.16 0 0 1-.29.635q-.225.237-.624.238l-13.248.01zM8.881 19q.714 0 1.197-.472.482-.472.482-1.14 0-.67-.482-1.142-.483-.472-1.197-.472-.693 0-1.181.472a1.53 1.53 0 0 0-.488 1.141q0 .67.488 1.141.488.472 1.18.472m9.248 0q.693 0 1.181-.472t.488-1.14-.488-1.142a1.64 1.64 0 0 0-1.18-.472q-.694 0-1.192.472a1.5 1.5 0 0 0-.499 1.141q0 .67.499 1.141.499.472 1.191.472"></path></svg>
-        </div>
-    </div>
-    
-    <div></div>
-</header>
+    </header>
+
+</div>
+
 
 <slot></slot>
