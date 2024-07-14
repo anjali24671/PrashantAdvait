46a405b1165146cc78518ae91f19ed617e8641ea
Anjali <holla2467@gmail.com>
2024-07-14 12:05:16 +0530

    style book detail page and make it responsive

--- src/routes/books/[book_id]/+page.svelte
+++ src/routes/books/[book_id]/+page.svelte
@@ -6,9 +6,9 @@
     export let data   
 
     let unsubscribe;
-    let eClicked=true
+    let eClicked=false
     let quantity = 1
-    let pClicked=false
+    let pClicked=true
     let initialUpdate = true;
     let book = JSON.parse(data.book)
 
@@ -48,9 +48,9 @@
 </script>
 
 <main>
-   <div class="flex m-5 justify-between gap-5">
+   <div class="flex m-5 justify-between gap-5 ">
       <!-- Book Image for large screens -->
-      <div class="min-w-44 hidden lg:flex lg:min-w-52 rounded-[5px] h-[300px] bg-orange-500">
+      <div class="min-w-44 hidden  lg:flex lg:min-w-52 rounded-[5px] h-[300px] ">
           <img class="lg:w-full self-center w-44 mx-auto" src="{book.photoURL}" alt="book image">
       </div>
       <div class="flex flex-col gap-6">
@@ -61,13 +61,13 @@
           </div>
 
           <!--price section for small screen-->
-          <div class=" w-full flex lg:w-44">
-            <div on:click={toggleEBookClass} class={`cursor-pointer py-2 pl-4 w-[50%] border-2  rounded-l-lg ${eClicked ? "bg-orange-100 border-orange-600" : "border-gray-300"}`}>
+          <div class="w-full lg:w-[40%] lg:min-w-[400px] md:gap-2 flex ">
+            <div on:click={toggleEBookClass} class={`cursor-pointer py-2 pl-4  w-[50%] border-2 md:rounded-lg rounded-l-lg ${eClicked ? "bg-orange-100 border-orange-600" : "border-gray-300"}`}>
                 <h2 class="font-semibold text-lg">eBook</h2>
                 <p class="text-xs font-semibold text-gray-700">Suggested Contribution</p>
                 <h1 class="text-orange-600 font-semibold text-xl">₹{book.price}</h1>
             </div>
-            <div on:click={togglePaperClass} class={`cursor-pointer py-2 pl-4 w-[50%] border-2 ml-[-1px] rounded-r-lg ${pClicked ? "bg-orange-100  border-orange-600" : "border-gray-300"}`}>
+            <div on:click={togglePaperClass} class={`cursor-pointer py-2 pl-4 w-[50%] border-2 md:rounded-lg ml-[-1px] rounded-r-lg ${pClicked ? "bg-orange-100  border-orange-600" : "border-gray-300"}`}>
                 <h2 class="font-semibold text-lg">Paperback</h2>
                
                 {#if book.paperPrice}
@@ -92,9 +92,9 @@
 
           {#if pClicked}
           <div class="md:hidden flex justify-between items-center ">
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2 ">
                 <h2>Quantity:</h2>
-                <div class="border items-center font-semibold flex gap-7 px-3 py-1">
+                <div class="border border-black-200 items-center shadow font-semibold flex gap-7 px-3 py-1">
                     <button on:click={()=> {
                         (quantity<=2)? quantity=0 : quantity--}} class="text-xl font-bold cursor-pointer">-</button>
                     <p>{quantity}</p>
@@ -111,12 +111,15 @@
 
           <!--checkout for small screen-->
          <div class="min-w-80 lg:hidden flex flex-col gap-4 rounded-[5px] ">
-            <div class="flex font-semibold justify-between">
+            <!-- <div class="flex font-semibold justify-between">
                 <div>Add to cart</div>
                 <div class="text-orange-600 ">GO TO CART</div>
-            </div>
-            <button class="hover:bg-orange-100 border  text-lg text-orange-600 rounded-lg py-2 border-orange-600">Get eBook now</button>
-            <button class="text-orange-600 text-lg">request echolarship</button>
+            </div> -->
+            <button class="hover:bg-orange-600 hover:text-white border  text-lg text-orange-600 rounded-lg py-2 border-orange-600">Add eBook to Cart</button>
+            <button class="hover:bg-orange-600 hover:text-white border  text-lg text-orange-600 rounded-lg py-2 border-orange-600">Get eBook now</button>
+            {#if eClicked}
+                <button class="text-orange-600 text-lg">request echolarship</button>
+            {/if}
          </div>
 
          <hr>
@@ -136,6 +139,64 @@
             <h2 class="text-lg font-semibold">Description</h2>
             <p class="text-gray-800">{book.description}</p>
          </div>
+      </div>
+
+
+      <!--checkout for large screen-->
+      <div class={`bg-orange-100 lg:flex hidden font-semibold  flex-col gap-3 p-5 min-w-[300px] ${(eClicked) ? ` max-h-[350px]`: 'max-h-[420px]' }`}>
+        <div class="flex mb-6 font-semibold justify-between">
+            {#if eClicked}
+                <div>Suggested Contribution</div>
+                <div class="text-orange-600 ">₹{book.price}</div>
+            {:else}
+                <div>Buy new:</div>
+                {#if book.paperPrice}
+                    <div class="text-orange-600 ">₹{book.paperPrice}</div>
+                {:else}
+                    <div class="text-orange-600 ">Not available</div>
+                {/if}
+            {/if}
+        </div>
+
+
+        <!--quantity section-->
+        {#if pClicked}
+          <div class=" flex flex-col justify-between items-start gap-3 ">
+            <div class="font-semibold flex items-center gap-3 justify-center">
+                <p class="text-green-600 text-lg">In Stock</p>
+                <p class="font-bold text-orange-600 text-sm">Free Delivery</p>
+            </div>
+            <div class="flex items-center  mb-5 gap-2">
+                <h2>Quantity:</h2>
+                <div class="shadow bg-orange-200 rounded-lg items-center font-semibold flex gap-7 px-3 py-1">
+                    <button on:click={()=> {
+                        (quantity<=2)? quantity=0 : quantity--}} class="text-xl font-bold cursor-pointer">-</button>
+                    <p>{quantity}</p>
+                    <button on:click={()=> {
+                        (quantity>=10)? quantity : quantity++}} class="text-xl font-bold cursor-pointer">+</button>
+                </div>
+            </div>
+            
+          </div>
+          {/if}
+        <button class="hover:bg-orange-700 border bg-orange-600 text-white text-md text-orange-600 rounded-lg py-2 border-orange-600">Add eBook to Cart</button>
+        <button class="hover:bg-orange-200 border  text-md text-orange-600 rounded-lg py-2 border-orange-600">Get eBook now</button>
+        {#if eClicked}
+            <button class=" text-md text-orange-600 py-2 ">REQUEST SCHOLARSHIP</button>
+        {/if}
+        
+
+        <div class="flex flex-col gap-7 justify-center items-center">
+            <p class="text-lg">Share this Book</p>
+            <div class="flex w-full justify-around">
+                <div><svg xmlns="http://www.w3.org/2000/svg" width="19" height="34" viewBox="0 0 19 34" class="h-7 w-7"><g fill="none" fill-rule="evenodd"><path d="M0 0h19v34H0z"></path><path fill="#3E6FA7" stroke="#3E6FA7" stroke-width="1.393" d="M12.074 33H6.23l-.133-14.297H1V13.13h5.102l-.005-4.52C6.096 4.02 9.3 1 14.16 1l3.84.23v5.296h-3.394c-1.892 0-2.532 1.342-2.532 2.81v3.795h5.666l-.765 5.572h-4.9z" style="--darkreader-inline-fill: #7ca8ce; --darkreader-inline-stroke: #7ca8ce;" data-darkreader-inline-fill="" data-darkreader-inline-stroke=""></path></g></svg></div>
+                <div><svg xmlns="http://www.w3.org/2000/svg" width="38" height="34" viewBox="0 0 38 34" class="h-7 w-7"><g fill="none" fill-rule="evenodd"><path d="M0 0h38v34H0z"></path><path fill="#08BBEE" d="M5.213 20.363a7.25 7.25 0 0 0 3.634-.251c-3.548-.578-6.262-3.748-6.262-7.572q0-.09.002-.178a7.2 7.2 0 0 0 3.551.995c-2.13-1.348-3.553-3.773-3.553-6.542a7.8 7.8 0 0 1 1.05-3.931c3.78 4.663 9.323 7.75 15.572 8.17a8 8 0 0 1-.175-1.683c0-4.347 3.415-7.871 7.629-7.871 2.201 0 4.184.962 5.577 2.5a11.8 11.8 0 0 0 4.735-1.898A7.4 7.4 0 0 1 33.728 6.4 12.5 12.5 0 0 0 38 5.19a12.9 12.9 0 0 1-3.712 4.008l.002.173-.004.896c0 12.279-9.647 22.233-21.549 22.233-4.329 0-8.36-1.317-11.737-3.584q.9.108 1.83.109c3.585 0 6.89-1.24 9.532-3.326-3.384 0-6.193-2.242-7.149-5.336" style="--darkreader-inline-fill: #32cbf7;" data-darkreader-inline-fill=""></path></g></svg></div>
+                <div><svg xmlns="http://www.w3.org/2000/svg" width="32" height="34" viewBox="0 0 32 34" class="h-7 w-7"><g fill="none" fill-rule="evenodd"><path d="M0 0h32v34H0z"></path><path fill="#06BA27" d="M15.218 1.019c9.364-.445 17.065 7.145 16.774 16.455-.271 8.685-7.533 15.584-16.24 15.452A16 16 0 0 1 9.4 31.509l-.524-.249L.208 33l1.534-8.787A15.9 15.9 0 0 1 0 17.013C-.026 8.503 6.698 1.423 15.218 1.02m.781 3.098a13.06 13.06 0 0 0-9.205 3.804 12.9 12.9 0 0 0-3.812 9.184c0 1.889.393 3.7 1.17 5.392l.245.504.576 1.125-1.084 5.248 5.147-1.217 1.17.58a12.9 12.9 0 0 0 5.793 1.355 12.94 12.94 0 0 0 9.204-3.804 13 13 0 0 0 3.813-9.183c0-3.47-1.354-6.731-3.812-9.184a12.95 12.95 0 0 0-9.205-3.804M9.985 9.543a1.158 1.158 0 0 1 1.729.269l.075.143 1.304 2.943c.147.331.128.709-.044 1.02l-.098.15-.662.854c-.279.36-.316.854-.09 1.249.788 1.368 3.872 4.104 5.338 4.696.37.15.788.088 1.1-.147l.128-.111.758-.77c.26-.265.628-.386.989-.334l.154.033 3.1.888a1.152 1.152 0 0 1 .654 1.727c-.606.955-1.568 2.12-2.771 2.41-2.121.514-5.38.013-9.447-3.775-3.522-3.28-4.453-6.01-4.233-8.177.125-1.23 1.15-2.338 2.016-3.068" style="--darkreader-inline-fill: #54f872;" data-darkreader-inline-fill=""></path></g></svg></div>
+                <div><svg xmlns="http://www.w3.org/2000/svg" width="35" height="34" viewBox="0 0 35 34" class="h-7 w-7"><g fill="none" fill-rule="evenodd"><path d="M0 0h35v34H0z"></path><path fill="#0A66C2" fill-rule="nonzero" d="M3.99 8.956c2.172 0 3.99-1.812 3.99-3.978S6.161 1 3.99 1 0 2.812 0 4.978s1.817 3.978 3.99 3.978M.665 33h6.65V10.9H.664zM25.71 10.282c-2.926 0-5.143 1.06-6.34 2.696V10.9h-6.649V33h6.65V20.934c0-3.138 1.684-4.553 4.122-4.553 2.128 0 3.857 1.282 3.857 4.022V33H34V19.43c0-5.966-3.812-9.148-8.29-9.148" style="--darkreader-inline-fill: #62b3f6;" data-darkreader-inline-fill=""></path></g></svg></div>
+            </div>
+        </div>
+       
+
       </div>
    </div>
    
