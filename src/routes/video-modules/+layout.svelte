742fad656e4a598ccd8139590c932f373281046b
Anjali <holla2467@gmail.com>
2024-07-28 10:59:35 +0530

    REFACT: search functionality for smaller sized screens

--- src/routes/video-modules/+layout.svelte
+++ src/routes/video-modules/+layout.svelte
@@ -64,15 +64,17 @@
                     {/each}
 
                 </select>
-                <input bind:value={inputQuery} class="w-full p-2 md:px-2 md:px-[40px] md:rounded-[0px] rounded-[8px]" placeholder="search for videos">
+                <input bind:value={inputQuery} class="w-full outline-none p-2 md:px-2 md:px-[40px] md:rounded-[0px] rounded-[8px]" placeholder="search for videos">
                 <button on:click={setQuery} class="md:bg-orange-200 p-2 md:block hidden absolute h-full md:right-0">
                     <svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16" class="fill-current laptop:h-4 laptop:w-4"><path fill-rule="evenodd" d="M6.5 2a4 4 0 1 0 0 8 4 4 0 0 0 0-8m-6 4a6 6 0 1 1 10.89 3.476l4.817 4.817a1 1 0 0 1-1.414 1.414l-4.816-4.816A6 6 0 0 1 .5 6"></path></svg>
                 </button>
 
-                <div class="md:hidden block self-center mx-2 ">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16" class="fill-current laptop:h-4 laptop:w-4"><path fill-rule="evenodd" d="M6.5 2a4 4 0 1 0 0 8 4 4 0 0 0 0-8m-6 4a6 6 0 1 1 10.89 3.476l4.817 4.817a1 1 0 0 1-1.414 1.414l-4.816-4.816A6 6 0 0 1 .5 6"></path></svg>
+            
+                <button on:click={setQuery} class="md:hidden block self-center  mx-2 ">
 
-                </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16" class="fill-current laptop:h-4 laptop:w-4"><path fill-rule="evenodd" d="M6.5 2a4 4 0 1 0 0 8 4 4 0 0 0 0-8m-6 4a6 6 0 1 1 10.89 3.476l4.817 4.817a1 1 0 0 1-1.414 1.414l-4.816-4.816A6 6 0 0 1 .5 6"></path></svg>
+                </button>
+        
 
             
                 
